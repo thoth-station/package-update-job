19bc5f323a472acf9c6f723c2404e0d6143f9fba
Kevin Postlethwait <k.postlethwait24@gmail.com>
2020-02-07 09:46:34 -0500

    Add metrics to package-update-job

--- app.py
+++ app.py
@@ -17,9 +17,13 @@
 
 """This is run periodically to ensure integrity of Python Packages stored in the database."""
 
+from prometheus_client import CollectorRegistry, Gauge, Counter, push_to_gateway
+
 from thoth.storages import GraphDatabase
 from thoth.python import Source
+
 import logging
+
 from messages.missing_package import MissingPackageMessage
 from messages.missing_version import MissingVersionMessage
 from messages.hash_mismatch import HashMismatchMessage
@@ -27,6 +31,41 @@ from messages.hash_mismatch import HashMismatchMessage
 _LOGGER = logging.getLogger(__name__)
 
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
+
+prometheus_registry = CollectorRegistry()
+
+_THOTH_METRICS_PUSHGATEWAY_URL = os.getenv("PROMETHEUS_PUSHGATEWAY_URL")
+
+thoth_metrics_exporter_info = Gauge(
+    "graph_sync_job_info",
+    "Thoth Graph Sync Job information",
+    ["version"],
+    registry=prometheus_registry,
+)
+thoth_metrics_exporter_info.labels(__version__).inc()
+
+_METRIC_MISSING_PACKAGE = Gauge(
+    "package_update_missing_package",
+    "Number of packages missing off of indexes.",
+    ["namespace"],
+    registry=prometheus_registry,
+)
+
+_METRIC_MISSING_VERSION = Gauge(
+    "package_update_missing_version",
+    "Number of individual versions messing off indexes.",
+    ["namespace"],
+    registry=prometheus_registry,
+)
+
+_METRIC_HASH_MISMATCH = Gauge(
+    "package_update_hash_mismatch",
+    "Number of package hash mismatches.",
+    ["namespace"],
+    registry=prometheus_registry,
+)
+
+namespace = os.getenv("THOTH_NAMESPACE")
 
 
 def main():
@@ -48,6 +87,7 @@ def main():
         if not src.provides_package(pkg[0]):
             removed_pkgs.add(f"{pkg[1]}_{pkg[0]}")
             missing_package.publish_to_topic(missing_package.MessageContents(index_url=pkg[1], package_name=pkg[0]))
+            _METRIC_MISSING_PACKAGE.inc(namespace=namespace)
             _LOGGER.debug("%r no longer provides %r", pkg[1], pkg[0])
 
     all_pkg_vers = graph.get_python_package_versions_all()
@@ -65,6 +105,7 @@ def main():
                     index_url=pkg_ver[2], package_name=pkg_ver[0], package_version=pkg_ver[1]
                 )
             )
+            _METRIC_MISSING_VERSION.inc(namespace=namespace)
             _LOGGER.debug("%r no longer provides %r-%r", pkg_ver[2], pkg_ver[0], pkg_ver[1])
             continue
 
@@ -74,6 +115,7 @@ def main():
             hash_mismatch.publish_to_topic(
                 hash_mismatch.MessageContents(index_url=pkg_ver[2], package_name=pkg_ver[0], package_version=pkg_ver[1])
             )
+            _METRIC_HASH_MISMATCH.inc(namespace=namespace)
             _LOGGER.debug("Source hashes:\n%r\nStored hashes:\n%r\nDo not match!", source_hashes, stored_hashes)
 
 
