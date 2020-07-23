f4f317aedab0cab0148a73e38c34e9f5505aed3f
Kevin Postlethwait <k.postlethwait24@gmail.com>
2020-07-23 09:23:51 -0400

    Use script from investigator

--- app.sh
+++ app.sh
@@ -1,23 +1,26 @@
+
 #!/usr/bin/env sh
 #
 # This script is run by OpenShift's s2i. Here we guarantee that we run desired
-# command
+# command and debug level
 #
 
-if [ "$SUBCOMMAND" = "producer" ]
-then
-    if [ "$FAUST_DEBUG" != "0" && "$FAUST_DEBUG" != "" ]
-    then
-        exec pipenv run faust --debug --loglevel debug -A producer main
-    else
-        exec pipenv run faust -A producer main
-    fi
-elif [ "$SUBCOMMAND" = "consumer" ]
-then
-    if [ "$FAUST_DEBUG" != "0" && "$FAUST_DEBUG" != "" ]
-    then
-        exec pipenv run faust --debug --loglevel debug -A consumer worker
-    else
-        exec pipenv run faust -A consumer worker
-    fi
+set -o nounset
+set -o errexit
+set -o errtrace
+set -o pipefail
+trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+
+SUBCOMMAND=${SUBCOMMAND:-producer}
+DEBUG_LEVEL=${DEBUG_LEVEL:-0}
+DEBUG_FLAGS=""
+
+if [ "$DEBUG_LEVEL" -eq 1 ]; then
+    DEBUG_FLAGS="--debug --loglevel debug"
+fi
+
+if [ "$SUBCOMMAND" == "producer" ]; then
+    exec faust ${DEBUG_FLAGS} -A producer main
+elif [ "$SUBCOMMAND" = "consumer" ]; then
+    exec faust ${DEBUG_FLAGS} -A consumer worker --web-host 0.0.0.0
 fi
