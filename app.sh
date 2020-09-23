935ab220a2ebb256834a994d4803a1d69705e16d
KPostOffice <kpostlet@redhat.com>
2020-09-23 02:14:42 -0400

    remove consumer logic (moved to investigator) (#130)
    * remove consumer logic (moved to investigator)
    
    * remove dependencies of consumer

--- app.sh
+++ app.sh
@@ -11,7 +11,6 @@ set -o errtrace
 set -o pipefail
 trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
 
-SUBCOMMAND=${SUBCOMMAND:-producer}
 DEBUG_LEVEL=${DEBUG_LEVEL:-0}
 DEBUG_FLAGS=""
 
@@ -19,8 +18,4 @@ if [ "$DEBUG_LEVEL" -eq 1 ]; then
     DEBUG_FLAGS="--debug --loglevel debug"
 fi
 
-if [ "$SUBCOMMAND" == "producer" ]; then
-    exec faust ${DEBUG_FLAGS} -A producer main
-elif [ "$SUBCOMMAND" = "consumer" ]; then
-    exec faust ${DEBUG_FLAGS} -A consumer worker --web-host 0.0.0.0
-fi
+exec faust ${DEBUG_FLAGS} -A producer main
